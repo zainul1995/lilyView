c70ec6a8ebfe2d05d64269720af853a35b8f3296
zainul1995 <zainul1995@gmail.com>
2020-05-09 14:38:01 +0530

    removed protect

--- dreamProject/viewRoutes.js
+++ dreamProject/viewRoutes.js
@@ -9,7 +9,7 @@ const viewRouter = express.Router();
 viewRouter.route('/').get(viewController.getOverview);
 
 viewRouter.route('/signup').get(viewController.getSignUpForm);
-viewRouter.use(userController.protect);
+// viewRouter.use(userController.protect);
 viewRouter.route('/login').get(viewController.getLoginForm)
 viewRouter.route('/dashboard').get(viewController.getDashBoard);
 viewRouter.route('/view-videos').get(viewController.getVideoScreen);
