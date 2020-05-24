dd12a2f175b0a05c9c8c4c73e51d9f6eed6cccd7
zainul1995 <zainul1995@gmail.com>
2020-05-24 11:03:05 +0530

    added userpoints data

--- dreamProject/viewRoutes.js
+++ dreamProject/viewRoutes.js
@@ -9,11 +9,11 @@ const viewRouter = express.Router();
 viewRouter.route('/').get(viewController.getOverview);
 
 viewRouter.route('/signup').get(viewController.getSignUpForm);
-// viewRouter.use(userController.protect);
-viewRouter.route('/login').get(userController.protect, viewController.getLoginForm)
+viewRouter.use(userController.protect);
+viewRouter.route('/login').get(viewController.getLoginForm);
 viewRouter.route('/dashboard').get(viewController.getDashBoard);
 viewRouter.route('/view-videos').get(viewController.getVideoScreen);
 viewRouter.route('/upload-video').get(viewController.getUploadScreen);
-viewRouter.route('/logout').get(userController.logout)
+viewRouter.route('/logout').get(userController.logout);
 
 module.exports = viewRouter;
